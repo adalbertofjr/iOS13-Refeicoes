85c70c8a353a1fbf59e51267c37a80be2c3b460b
Adalberto Fernandes Jr <adalberto.fernandesjr@gmail.com>
2021-01-01 18:32:16 -0300

    Refatorando print de informações

--- eggplant-brownie/Controller/ViewController.swift
+++ eggplant-brownie/Controller/ViewController.swift
@@ -9,17 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet var nomeTextField:UITextField!
-    @IBOutlet var felicidadeTextField:UITextField!
+    @IBOutlet var nomeTextField:UITextField?
+    @IBOutlet var felicidadeTextField:UITextField?
     
     @IBAction func adicionar(_ sender: Any){
-        let nome = nomeTextField.text
-        let felicidae = felicidadeTextField.text
+//        if let nomeRefeicao = nomeTextField?.text,
+//           let felicidadeRefeicao = felicidadeTextField?.text{
+//            let nome = nomeRefeicao
+//            if let felicidade = Int(felicidadeRefeicao){
+//                let refeicao = Refeicao(nome: nome, felicidade: felicidade)
+//
+//                print("comi \(refeicao.nome) e fiquei com felicidade: \(refeicao.felicidade)")
+//            }else{
+//                print("Erro ao adicionar refeição")
+//            }
+//        }
         
-        print("comi \(nome) e ficquei com felicidade: \(felicidae)")
+        guard let nome = nomeTextField?.text else {
+            return
+        }
+        
+        guard let felicidadeRefeicao = felicidadeTextField?.text,
+              let felicidade = Int(felicidadeRefeicao) else {
+            return
+        }
+        
+        let refeicao = Refeicao(nome: nome, felicidade: felicidade)
+        
+        print("comi \(refeicao.nome) e fiquei com felicidade: \(refeicao.felicidade)")
+        
     }
-    
-
-
 }
 
