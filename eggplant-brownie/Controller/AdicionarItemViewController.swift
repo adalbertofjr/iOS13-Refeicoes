1ba0b32630fefbd7a85dda084567f240068e4c8c
Adalberto Fernandes Jr <adalberto.fernandesjr@gmail.com>
2021-01-02 12:35:15 -0300

    Criando objeto Item com dados do formulário  para adição

--- eggplant-brownie/Controller/AdicionarItemViewController.swift
+++ eggplant-brownie/Controller/AdicionarItemViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class AdicionarItemViewController: UIViewController {
 
+    // MARK: - IBOutlets
+    @IBOutlet weak var nomeTextField: UITextField!
+    @IBOutlet weak var caloriasTextField: UITextField!
+    
+    
     // MARK: - View life cycle
     
     override func viewDidLoad() {
@@ -19,7 +24,19 @@ class AdicionarItemViewController: UIViewController {
     //MARK: - IBActions
     
     @IBAction func adicionarItem(_ sender: Any) {
-        navigationController?.popViewController(animated: true)
+        guard let nome = nomeTextField.text,
+              let calorias = caloriasTextField.text else {
+            return
+        }
+        
+        if let numDeCalorias = Double(calorias){
+            let item = Item(nome: nome, calorias: numDeCalorias)
+            print("item: \(item.nome) calorias: \(item.calorias)")
+            
+            navigationController?.popViewController(animated: true)
+        }
+        
+       
     }
     
 }
